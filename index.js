e5f7b0fffe787658572369f315fb8c7a947fb8fa
Bjornskjald <github@bjorn.ml>
2018-06-28 22:55:00 +0200

    Made client work (hopefully)

--- index.js
+++ index.js
@@ -4,16 +4,32 @@ const PROTO_PATH = path.join(__dirname, 'messages.proto')
 
 const messageProto = grpc.load(PROTO_PATH).chatplug;
 
-const client = new messageProto.MessagesService(
-  'localhost:2137',
-  grpc.credentials.createInsecure()
-)
-client.sendMessage({
-  message: 'Message sent from grpc api. Yay!',
-  attachments: [],
-  author: { username: 'ElonMusk' },
-  origin: { id: 'dupa', service: 'grpc', name: 'rpcsamplethread' },
-}, (err, response) => {
-  if (err) return console.error(err)
-  console.log(response)
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
 })
+
+login().then(async client => {
+  const username = await ask('Enter username [ChatPlugCLI]:') || 'ChatPlugCLI'
+  rl.on('line', message => {
+    client.sendMessage({
+      message,
+      attachments: [],
+      author: { username },
+      origin: { id: 'chatplugcli', service: 'grpc', name: 'ChatPlugCLI' },
+    }, (err, response) => {
+      if (err) return console.error(err)
+      console.log(response)
+    })
+  })
+})
+
+async function login () {
+  const host = await ask('Enter host [localhost]:') || 'localhost'
+  const port = await ask('Enter port [2137]:') || '2137'
+  return new messageProto.MessagesService(`${host}:${port}`, grpc.credentials.createInsecure())
+}
+
+function ask (question) {
+  return new Promise(resolve => rl.question(question, resolve))
+}
