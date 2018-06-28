fff2262be48c1b4c7ba9cd38048d2dd41f6480db
Bjornskjald <github@bjorn.ml>
2018-06-28 23:11:54 +0200

    added protos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const grpc = require('grpc')
-const PROTO_PATH = path.join(__dirname, 'messages.proto')
+const PROTO_PATH = path.join(__dirname, 'protos', 'messages.proto')
 
 const messageProto = grpc.load(PROTO_PATH).chatplug;
 
